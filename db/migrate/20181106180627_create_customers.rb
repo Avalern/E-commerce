55ec824396fa27008fbefbbbe6a86b4ba01a21fe
Alex <dashex2@gmail.com>
2018-11-06 18:08:35 +0000

    Added a Customer model.

---
+++ db/migrate/20181106180627_create_customers.rb
@@ -0,0 +1,13 @@
+class CreateCustomers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :customers do |t|
+      t.int :cust_id
+      t.string :name
+      t.string :email
+      t.string :address
+      t.references :province, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
