297810a39f7ecaf5ce89f831776b4d0a2080c80b
Alex <dashex2@gmail.com>
2018-11-09 20:34:01 +0000

    Fixed my image upload bug

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,4 +3,10 @@ class Album < ApplicationRecord
   has_many :ordered_albums
   has_many :orders, through: :ordered_albums
   mount_uploader :image, ImageUploader
+
+  validates :album_id, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :artist_name, presence: true
 end
