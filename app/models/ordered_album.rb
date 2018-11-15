1841d892ee47bb9ed67cb85f71e4d6197be36d92
Alex <dashex2@gmail.com>
2018-11-15 21:17:42 +0000

    Added few validations to ordered_album table

--- app/models/ordered_album.rb
+++ app/models/ordered_album.rb
@@ -1,4 +1,8 @@
 class OrderedAlbum < ApplicationRecord
   belongs_to :order
   belongs_to :album
+
+  validates :quantity,  presence: true
+  validates :order_id, presence: true
+  validates :album_id, presence: true
 end
