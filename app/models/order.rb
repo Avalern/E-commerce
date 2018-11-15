8ddc2c804397a43d0ab37420742088866531c8fc
Alex <dashex2@gmail.com>
2018-11-15 21:19:42 +0000

    Added few validations for Order table

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,9 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :ordered_albums
   has_many :customers, through: :ordered_albums
+
+  validates :order_id,  presence: true
+  validates :amount_due, presence: true
+  validates :customer_id, presence: true
+
 end
