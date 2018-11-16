febea43b10b8735a65fb6b0cc3102871765aad8e
Alex <dashex2@gmail.com>
2018-11-16 17:32:39 +0000

    Created a migration to add a column to orders table

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :ordered_albums
-  has_many :customers, through: :ordered_albums
+  has_many :albums, through: :ordered_albums
 
   validates :order_id,  presence: true
   validates :amount_due, presence: true
